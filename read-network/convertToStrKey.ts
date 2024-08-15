7149a15f5b1532325165d163614964789466294c
ivansing <lancedealway@gmail.com>
2024-08-14 21:30:06 -0500

    modified convertToStrKey.ts

--- read-network/convertToStrKey.ts
+++ read-network/convertToStrKey.ts
@@ -2,13 +2,11 @@ import {PublicKey} from "@solana/web3.js"
 
 const publicKeyUintArray = new Uint8Array([
     
-        118, 152, 209, 254, 141, 247, 104,
-         41, 238,  25,  54, 175, 171, 227,
-        178, 142, 156, 242, 176, 166, 219,
-        171,  96,  30,  10,  94, 188,  82,
-         95, 248, 156, 126
+        //<your public key>
       
 ])
 
 const publicKey = new PublicKey(publicKeyUintArray)
 console.log(publicKey.toBase58())
+
+// generated key
