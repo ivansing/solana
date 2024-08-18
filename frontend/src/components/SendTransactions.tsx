c5b8da0f1d26a24fff5f027090a986b26f8b9dd9
ivansing <lancedealway@gmail.com>
2024-08-17 22:08:02 -0500

    added folder frontend and build an NextJS visual functions and components

---
+++ frontend/src/components/SendTransactions.tsx
@@ -0,0 +1,22 @@
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import * as web3 from '@solana/web3.js';
+
+const {publicKey, sendTransaction} = useWallet();
+const {connection} = useConnection();
+
+const sendSol = async (event: any ) => {
+    event.preventDefault();
+
+    const transaction = new web3.Transaction();
+    const recipientPubKey = new web3.PublicKey(event.target.recipient.value);
+
+    const sendSolInstruction = web3.SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: recipientPubKey,
+        lamports: 0.1 * web3.LAMPORTS_PER_SOL,
+    });
+
+    transaction.add(sendSolInstruction);
+    const signature = sendTransaction(transaction, connection)
+    console.log(signature)
+}
---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css'
+
+function MyApp({ Component, pagePros}) {
+  return <Component {...pagePros} />
+}
+
+export default MyApp
+
+
+
+
+
+
+
